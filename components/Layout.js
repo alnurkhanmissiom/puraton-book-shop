b67ee4f573cba57cd66ef6994cd65b7253c34d97
alnurkhanmissiom <alnurkhanmissiom@gmail.com>
2022-08-15 01:33:58 +0600

    middle

--- components/Layout.js
+++ components/Layout.js
@@ -90,7 +90,7 @@ export default function Layout({ title, children }) {
             </div>
           </nav>
         </header>
-        <main className="container m-auto ml-0 mt-4 px-4"> {children} </main>
+        <main className="container m-auto mt-4 px-4"> {children} </main>
         <footer className="flex h-10 justify-center items-center shadow-inner">
           Copyright@2022 puraton-boighor
         </footer>
